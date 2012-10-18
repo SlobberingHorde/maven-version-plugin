bcd9bd4ad0fe3d599a63be4a82eb9d6dbe7d54d4
Jerry Hoerig <jerryhoerig@gmail.com>
2012-10-17 22:55:05 -0400

    Updates to license and project configuration

--- src/main/java/com/blackboxsoft/maven/VersionMojo.java
+++ src/main/java/com/blackboxsoft/maven/VersionMojo.java
@@ -1,8 +1,35 @@
 package com.blackboxsoft.maven;
 
+/*
+ * #%L
+ * maven-version-plugin
+ * %%
+ * Copyright (C) 2012 BlackBox Software, Inc
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,7 +72,7 @@ public class VersionMojo extends AbstractMojo {
      * @parameter expression="${version.types}" default-value="txt"
 	 */
 	private List<String> types;
-	
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		writeHeader();		writeTheVersionFiles(createFileContents());
@@ -109,19 +136,19 @@ public class VersionMojo extends AbstractMojo {
 		getLog().info(message);
 	}
 
-	public void setProject(String project) {
-		this.project = project;
+	public String getProject() {
+		return project;
 	}
 
-	public void setVersion(String version) {
-		this.version = version;
+	public String getVersion() {
+		return version;
 	}
 
-	public void setBranch(String branch) {
-		this.branch = branch;
+	public String getBranch() {
+		return branch;
 	}
 
-	public void setTypes(List<String> types) {
-		this.types = types;
+	public List<String> getTypes() {
+		return types;
 	}
 }
